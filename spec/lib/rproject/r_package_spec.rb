fd9a10993e8d19a1d104184942209c210e398395
Bruno Grasselli <bruno.grasselli@gmail.com>
2013-08-29 15:57:36 +0200

    changing the test example

--- spec/lib/rproject/r_package_spec.rb
+++ spec/lib/rproject/r_package_spec.rb
@@ -40,19 +40,19 @@ NeedsCompilation: no
   describe "#description" do
     before do
       body = %{
-Package: ABCExtremes
-Version: 1.0
+Package: TExPosition
+Version: 2.0.2
 Depends: SpatialExtremes, combinat
 License: GPL-2
 NeedsCompilation: no
       }
       stub_request(:get, "http://cran.r-project.org/src/contrib/PACKAGES").to_return({body: body})
-      stub_request(:get, "http://cran.r-project.org/src/contrib/ABCExtremes_1.0.tar.gz").to_return({body: File.read('./spec/support/data/ABCExtremes_1.0.tar.gz')})
+      stub_request(:get, "http://cran.r-project.org/src/contrib/TExPosition_2.0.2.tar.gz").to_return({body: File.read('./spec/support/data/TExPosition_2.0.2.tar.gz')})
     end
 
     it "returns package description" do
       package = described_class.all.first
-      package.description.should eq %{This package contains code to 1.) construct the summary statistic needed to facilitate approximate Bayesian computing for spatial extremes, and then 2.) run the ABC-Rejection method to produce a set of draws from the ABC posterior.  The intention is for the user to directly modify the command >abc.rej() to suit his or her purposes.  In general, ABC fitting of max-stable processes is very computationally expensive, and requires advanced programming skills with research computing.}
+      package.description.should eq %{TExPosition is an extension of ExPosition for two table analyses, specifically, discriminant analyses.}
     end
   end
 end
