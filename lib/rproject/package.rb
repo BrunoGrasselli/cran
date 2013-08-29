c8af181c5e802b49a3237789bd31aa1e1d7045ff
Bruno Grasselli <bruno.grasselli@gmail.com>
2013-08-29 12:06:50 +0200

    loading and showing description

--- lib/rproject/package.rb
+++ lib/rproject/package.rb
@@ -1,19 +1,37 @@
-require "open-uri"
-require "dcf"
+require 'open-uri'
+require 'dcf'
+require 'rubygems/package'
+require 'zlib'
 
 module RProject
   class Package
-    URL = "http://cran.r-project.org/src/contrib/PACKAGES"
+    CRAN_URL = 'http://cran.r-project.org/src/contrib'
 
-    attr_reader :name
+    attr_reader :name, :version
 
     def self.all
-      body = open(URL).read
+      body = open("#{CRAN_URL}/PACKAGES").read
       Dcf.parse(body.strip).map {|content| new(content)}
     end
 
     def initialize(attributes={})
       @name = attributes['Package']
+      @version = attributes['Version']
+    end
+
+    def description
+      @description ||= load_description
+    end
+
+    private
+
+    def load_description
+      source = open("#{CRAN_URL}/#{name}_#{version}.tar.gz")
+      tar = Gem::Package::TarReader.new(Zlib::GzipReader.new(source))
+      tar.rewind
+      description_file = ''
+      tar.each { |entry| description_file = Dcf.parse(entry.read.strip).first if entry.full_name.include? "DESCRIPTION" }
+      @description = description_file['Description']
     end
   end
 end
--- spec/lib/rproject/package_spec.rb
+++ spec/lib/rproject/package_spec.rb
@@ -30,9 +30,29 @@ NeedsCompilation: no
     end
   end
 
-  describe "#name" do
-    it "returns package name" do
-      described_class.new('Package' => 'Test').name.should eq 'Test'
+  describe "attributes" do
+    subject { described_class.new('Package' => 'Test', 'Version' => '1.2.3') }
+
+    its(:name)    { should eq 'Test' }
+    its(:version) { should eq '1.2.3' }
+  end
+
+  describe "#description" do
+    before do
+      body = %{
+Package: ABCExtremes
+Version: 1.0
+Depends: SpatialExtremes, combinat
+License: GPL-2
+NeedsCompilation: no
+      }
+      stub_request(:get, "http://cran.r-project.org/src/contrib/PACKAGES").to_return({body: body})
+      stub_request(:get, "http://cran.r-project.org/src/contrib/ABCExtremes_1.0.tar.gz").to_return({body: File.read('./spec/support/data/ABCExtremes_1.0.tar.gz')})
+    end
+
+    it "returns package description" do
+      package = described_class.all.first
+      package.description.should eq %{This package contains code to 1.) construct the summary statistic needed to facilitate approximate Bayesian computing for spatial extremes, and then 2.) run the ABC-Rejection method to produce a set of draws from the ABC posterior.  The intention is for the user to directly modify the command >abc.rej() to suit his or her purposes.  In general, ABC fitting of max-stable processes is very computationally expensive, and requires advanced programming skills with research computing.}
     end
   end
 end
