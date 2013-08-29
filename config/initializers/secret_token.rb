c54c88674dad0c9bfee5a1718ceb84adbb7a9216
Bruno Grasselli <bruno.grasselli@gmail.com>
2013-08-29 10:29:27 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cran::Application.config.secret_token = '3d6ffba6070cb5d608f5a95bf018373407daee8191104a48fc22a25cfd9e52885b2d5a8922691b3af940c8715a692a9a483c7107592ab49ed77810189a25a171'
